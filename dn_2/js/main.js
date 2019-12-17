940cbadb32bb0ef73870f69f4d041c43ad65553b
Filip Zeman <Filip.zeman@protonmail.com>
2019-12-17 13:54:35 +0200

    асд

--- dn_2/js/main.js
+++ dn_2/js/main.js
@@ -1,11 +1,37 @@
 $(function () {
+
   var nav = $('header nav');
   var button = $('.menu');
-  button.on('click', function () {
-    nav.toggleClass('hidden');
+
+  function navToggle() {
+    nav.toggle();
     button.toggleClass('close');
+  };
+
+  button.on('click', function () {
+    navToggle();
   });
-  $(".item").on('click', function () {
-    $(this).children(".li").toggleClass('hidden');
+
+  $(document).on("click", function (event) {
+    if ($("header nav").is(":visible") && !$(event.target).closest("header nav").length && !$(event.target).closest(".menu").length) {
+      $("header nav:visible").toggle();
+      button.toggleClass('close');
+    }
   });
+
+  var garage = $('header .garage_info');
+  var buttongarage = $('header .garage');
+
+  buttongarage.on('click', function () {
+    garage.toggle();
+  });
+
+  $(".garage_info").hide();
+
+  $(document).on("click", function (event) {
+    if (!$(event.target).closest("header .garage_info").length && !$(event.target).closest("header .garage").length) {
+      $("header .garage_info:visible").toggle();
+    }
+  });
+
 });
