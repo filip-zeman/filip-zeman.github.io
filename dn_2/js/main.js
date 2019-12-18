d5db358fa12059df2d81e428ccb9e93da44bbd84
Filip Zeman <Filip.zeman@protonmail.com>
2019-12-18 20:47:21 +0200

    asd

--- dn_2/js/main.js
+++ dn_2/js/main.js
@@ -35,8 +35,8 @@ $(function () {
     }
   });
 
-  $(window).scroll(function () {
-    var x = $(this).scrollTop();
-    $('.fav_categories_grid a div').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
-  });
+  //$(window).scroll(function () {
+  //  var x = $(this).scrollTop();
+  //  $('.fav_categories_grid a div').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
+  //});
 });
