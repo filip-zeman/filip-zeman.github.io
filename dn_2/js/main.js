7edaf474fefd4bb1bc572d462eace665e3542d29
Filip Zeman <Filip.zeman@protonmail.com>
2019-12-20 23:49:25 +0200

    azz

--- dn_2/js/main.js
+++ dn_2/js/main.js
@@ -12,12 +12,16 @@ $(function () {
     navToggle();
   });
 
-  $(document).on("click", function (event) {
-    if ($("header nav").is(":visible") && !$(event.target).closest("header nav").length && !$(event.target).closest(".menu").length) {
-      $("header nav:visible").toggle();
-      button.toggleClass('close');
-    }
-  });
+  if ($(window).width() < 768) {
+
+    $(document).on("click", function (event) {
+      if ($("header nav").is(":visible") && !$(event.target).closest("header nav").length && !$(event.target).closest(".menu").length) {
+        $("header nav:visible").toggle();
+        button.toggleClass('close');
+      }
+    });
+
+  }
 
   var garage = $('header .garage_info');
   var buttongarage = $('header .garage');
@@ -28,15 +32,10 @@ $(function () {
   });
 
   $(".garage_info").hide();
-
+  
   $(document).on("click", function (event) {
     if (!$(event.target).closest("header .garage_info").length && !$(event.target).closest("header .garage").length) {
       $("header .garage_info:visible").toggle();
     }
   });
-
-  //$(window).scroll(function () {
-  //  var x = $(this).scrollTop();
-  //  $('.fav_categories_grid a div').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
-  //});
 });
